b018cc4ac1944ead5a9ba328f9519bdfd82ae50c
Polycarp <61562996+Polycarp29@users.noreply.github.com>
2024-09-03 23:41:36 +0300

    Update

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-$con = mysqli_connect("localhost","root","","allphptricks");
+$con = mysqli_connect("localhost","root","","");
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
